1be481a9a4fbcaaf3d8a58de5bc0e293619811d9
PostUlatKojun <PostUlatKojun@gmail.com>
2017-09-13 18:33:58 +0300

    add install rsync for synchronize

--- provisions/provision_master.sh
+++ provisions/provision_master.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update
-apt-get install -y python-pip python-software-properties software-properties-common python-jinja2 python-markupsafe yum
+apt-get install -y python-pip python-software-properties software-properties-common python-jinja2 python-markupsafe yum rsync
 # pip install ansible
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
